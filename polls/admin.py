03d5920267d571a958e0bd876b91d2393fd7af96
Leo-nid <s19b2_ichalov@179.ru>
2021-06-24 18:43:09 +0300

    deleted duplication

--- polls/admin.py
+++ polls/admin.py
@@ -5,17 +5,18 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.conf.urls import url
 from .views import upload_file
 
-class PostAdmin(admin.ModelAdmin):
+class ImportExportAdmin(admin.ModelAdmin): # kinda like abstract
     change_list_template = "admin/monitor_change_list.html"
     list_display = ['view_title']
 
     def get_urls(self):
-        urls = super(PostAdmin, self).get_urls()
+        urls = super(ImportExportAdmin, self).get_urls()
         custom_urls = [
             url('^import/$', upload_file, name='process_import'),
-            url('^export/$', self.export_as_json, name='process_export'),]
+            url('^export/$', self.export_as_json, name='process_export'), ]
         return custom_urls + urls
 
+class PostAdmin(ImportExportAdmin):
     @admin.display(empty_value='???')
     def view_title(self, obj):
         return obj.post_text[:10] # first 10 symbols of text
@@ -27,22 +28,8 @@ class PostAdmin(admin.ModelAdmin):
         serializers.serialize("json", data, stream=response)
         return response
 
-    def import_from_json(self, request):
 
-        return HttpResponseRedirect("../")
-
-
-class CommentAdmin(admin.ModelAdmin):
-    change_list_template = "admin/monitor_change_list.html"
-    list_display = ['view_title']
-
-    def get_urls(self):
-        urls = super(CommentAdmin, self).get_urls()
-        custom_urls = [
-            url('^import/$', upload_file, name='process_import'),
-            url('^export/$', self.export_as_json, name='process_export'),]
-        return custom_urls + urls
-
+class CommentAdmin(ImportExportAdmin):
     @admin.display(empty_value='???')
     def view_title(self, obj):
         return obj.comment_text[:10]
@@ -53,12 +40,6 @@ class CommentAdmin(admin.ModelAdmin):
         response['Content-Disposition'] = 'attachment; filename=DownloadedText.json'
         serializers.serialize("json", data, stream=response)
         return response
-
-    def import_from_json(self, request):
-        Post.objects.all().delete()
-        it = serializers.deserialize("json", request.body)
-        it.save()
-        return HttpResponseRedirect("../")
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
